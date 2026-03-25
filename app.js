92ed39b11071f6f2d45f8cff9cc6cb35c815caa8
Janhavi Pradeep Sonawane <janhavisonawane171@gmail.com>
2026-03-25 16:32:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-// Step1 = keypress->game start
-//Step2 = btnflash+Level1
-//step3 = gameseq[],userseq[] which refill again at each level
-//Step4 = user press btn seq=check userseq[]==gameseq[] then Level Up,else GameOver
-
 let gameSeq= [];
 let userSeq= [];
 let highScore=0;
@@ -16,7 +11,7 @@ let h2=document.querySelector("h2");
 document.addEventListener("keypress", function(){
     if(started == false){
         console.log("game is started");
-        started = true;//only one time the game can start as when it is false at that time only
+        started = true;
         levelUp();
     }
 });
@@ -34,9 +29,6 @@ function levelUp(){
     let randIdx=Math.floor(Math.random()*3);
     let randColor=btns[randIdx];
     let randBtn=document.querySelector(`.${randColor}`);
-    // console.log(randIdx);
-    // console.log(randColor);
-    // console.log(randBtn);
     btnFlash(randBtn);
     gameSeq.push(randColor);
     console.log(gameSeq);
@@ -61,13 +53,10 @@ function checkAns(idx){
 }
 
 function btnPress(){
-    // Here the work should come which should happen when btn was pressed
     let btn=this;
     btnFlash(btn);
     let userColor=btn.getAttribute("id");
-    // console.log(userColor);
     userSeq.push(userColor);
-    // console.log(userSeq);
     checkAns(userSeq.length-1);
 }
 
